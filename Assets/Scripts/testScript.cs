76c0ef5ce6da2ca760f2fefdc8036936baced998
Matthew Pohlmann <pohlmann@usc.edu>
2014-01-24 20:44:48 -0800

    Added initial game framework

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//Timer.Instance.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			Timer.Instance.Add("Timer",
+			                   Foo,
+			                   1.0f,
+			                   true);
+		}
+	}
+
+	void Foo () {
+		Debug.Log("Foo");
+	}
+}
