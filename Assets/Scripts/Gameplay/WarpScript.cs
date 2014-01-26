50d4f5facddc81a08032d026660ad2fb8cfafa9f
Valakor <pohlmann@usc.edu>
2014-01-26 01:09:41 -0800

    warp script

--- Assets/Scripts/Gameplay/WarpScript.cs
+++ Assets/Scripts/Gameplay/WarpScript.cs
@@ -5,7 +5,11 @@ public class WarpScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.CompareTag("Player")){
-			//other.gameObject.
+			Destroy(other.transform.parent.FindChild("Vision").gameObject);
+			other.transform.GetComponent<Animator>().SetInteger("Facing", 6);
+			other.transform.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			other.transform.GetComponent<PlayerMove>().enabled = false;
+			other.transform.position = transform.position + Vector3.up * 0.35f;
 			GameManager.Instance.LevelEnd();
 			AudioManager.Instance.PlaySoundEffect("levelEnd", gameObject);
 		}
