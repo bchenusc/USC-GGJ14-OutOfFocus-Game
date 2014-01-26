a5bffef71a9f7e5d18f41ea5668a6763b8458e60
Jeffrey Chau <chaujeff@usc.edu>
2014-01-25 17:41:51 -0800

    Adding platform script

--- Assets/Scripts/Gameplay/PlatformScript.cs
+++ Assets/Scripts/Gameplay/PlatformScript.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlatformScript : MonoBehaviour {
 
+	public bool active;
+
 	// Use this for initialization
 	void Start () {
-	
+		active = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (active) {
+			//Move and do stuff
+		}
 	}
 }
