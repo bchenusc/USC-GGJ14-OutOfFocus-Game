8198e070ef2b5e992109d65a15e608e91370f81a
Mr Fluffy Sheep <brianych@usc.edu>
2014-01-25 22:29:35 -0800

    100th bitch

---
+++ Assets/Scripts/Gameplay/FOV.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FOV : MonoBehaviour {
+	
+	public Transform pivot;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = pivot.position;
+
+		Vector3 playerToMouse =  Vector3.Normalize(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+	}
+}
