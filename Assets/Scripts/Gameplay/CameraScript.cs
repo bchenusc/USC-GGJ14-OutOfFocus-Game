5776c55e82f6718d642bf45c5ef1a65b05c8e47a
Valakor <pohlmann@usc.edu>
2014-01-25 22:33:58 -0800

    making level 1 beautiful

---
+++ Assets/Scripts/Gameplay/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	Transform player;
+	
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+	}
+}
