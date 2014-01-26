e8f87962d814c577b607adc3abdc6076f4049bda
Mr Fluffy Sheep <brianych@usc.edu>
2014-01-25 22:52:28 -0800

    herroness

--- Assets/Scripts/Gameplay/FOV.cs
+++ Assets/Scripts/Gameplay/FOV.cs
@@ -9,6 +9,17 @@ public class FOV : MonoBehaviour {
 	void Update () {
 		transform.position = pivot.position;
 
-		Vector3 playerToMouse =  Vector3.Normalize(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+		Vector3 playerToMouse =  Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		playerToMouse.z = 0;
+		playerToMouse = Vector3.Normalize (playerToMouse);
+		//Axis is Vector z
+
+		float angle = Mathf.Acos ((Vector3.Dot (playerToMouse, new Vector3(1,0,0) )));
+		Debug.Log (angle);
+
+		Quaternion rotation = Quaternion.AngleAxis (angle * 180 / Mathf.PI  - 90, Vector3.forward);
+
+		transform.rotation = rotation;
+
 	}
 }
